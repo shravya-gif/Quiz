a5136df8c57b9a5261cc58fecc86ec7320918659
shravya-gif <95952294+shravya-gif@users.noreply.github.com>
2024-01-10 21:34:12 +0530

    NewQuiz

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,6 +1,13 @@
 import React from 'react'
-
+import axios from 'axios'
 export default function Quiz() {
+    const getQuiz=async()=>{
+        try {
+           const response= await axios.get()
+        } catch (error) {
+            console.log(error);
+        }
+    }
   return (
     <div className='flex justify-center items-center h-screen'>
       <h1>hii</h1>
